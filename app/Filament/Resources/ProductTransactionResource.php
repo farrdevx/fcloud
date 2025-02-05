75f9855e58f78257a5c3470af941dc8b7834c904
Jafar Faisal <farrdevx@gmail.com>
2025-02-05 18:36:01 +0700

    ProductTransactionResource Complte

--- app/Filament/Resources/ProductTransactionResource.php
+++ app/Filament/Resources/ProductTransactionResource.php
@@ -8,8 +8,11 @@ use App\Models\External;
 use App\Models\Package;
 use App\Models\ProductTransaction;
 use App\Models\PromoCode;
+use Faker\Provider\Text;
+use Filament\Actions\ViewAction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -27,12 +30,12 @@ class ProductTransactionResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Wizard::make([
-                    Forms\Components\Wizard\Step::make('Product and Price')
-                    ->schema([
-
-                        Forms\Components\Grid::make(2)
+                    Forms\Components\Fieldset::make('Payment Information')
+                ->schema([
+                        //Make a Grid 2 Column
+                        Forms\Components\Grid::make(2)->extraAttributes(['class' => 'w-4/4'])
                         ->schema([
+                            //Get & Make Package Selection For Product Transactions
                             Forms\Components\Select::make('package_id')
                             ->relationship('package', 'name')
                             ->searchable()
@@ -43,8 +46,9 @@ class ProductTransactionResource extends Resource
 
                                 $package = Package::find($state);
                                 $price = $package ? $package->price : 0;
-                                $quantitiy = $get('quantity') ?? 1;
-                                $subTotalAmount = $price * $quantitiy;
+                                $quantity = $get('quantity') ?? 1;
+                                $eprice = $get('eprice');
+                                $subTotalAmount = $price * $quantity;
 
                                 $set('price', $price);
                                 $set('sub_total_amount', $subTotalAmount);
@@ -53,6 +57,29 @@ class ProductTransactionResource extends Resource
                                 $grandTotalAmount = $subTotalAmount - $discount;
                                 $set('grand_total_amount', $grandTotalAmount);
                             }),
+                            //Get External Id Form For Product Transaction
+                            Forms\Components\Select::make('external_id')
+                                ->relationship('external', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->live()
+                                ->afterStateUpdated(function ($state, callable $get, callable $set){
+
+                                    $external = External::find($state);
+                                    $price = $get('price');
+                                    $eprice = $external ? $external->eprice : 0;
+                                    $quantity = $get('quantity') ?? 1;
+                                    $subTotalAmount = $eprice * $quantity;
+
+                                    $set('eprice', $eprice);
+                                    $set('sub_total_amount', $subTotalAmount);
+
+                                    $discount = $get('discount_amount') ?? 0;
+                                    $grandTotalAmount = $subTotalAmount - $discount;
+                                    $set('grand_total_amount', $grandTotalAmount);
+                                }),
+
+                            //Quantity Form On Product Transaction
                             Forms\Components\TextInput::make('quantity')
                                 ->required()
                                 ->numeric()
@@ -60,8 +87,9 @@ class ProductTransactionResource extends Resource
                                 ->live()
                                 ->afterStateUpdated(function ($state, callable $get, callable $set){
                                     $price = $get('price');
+                                    $eprice = $get('eprice');
                                     $quantity = $state;
-                                    $subTotalAmount = $price * $quantity;
+                                    $subTotalAmount = $price + $eprice * $quantity;
 
                                     $set('sub_total_amount', $subTotalAmount);
 
@@ -70,14 +98,93 @@ class ProductTransactionResource extends Resource
                                     $set('grand_total_amount', $grandTotalAmount);
                                 }),
 
-                                Forms\Components\Select::make('external_id')
-                                    ->relationship('external', 'name')
-                                    ->searchable()
-                                    ->preload(),
+                            //Make & Get Forms For Promo Code Function
+                            Forms\Components\Select::make('promo_code_id')
+                            ->relationship('promoCode', 'code')
+                            ->searchable()
+                            ->preload()
+                            ->live()
+                            ->afterStateUpdated(function ($state, callable $get, callable $set){
+                                $subTotalAmount = $get('sub_total_amount');
+                                $promoCode = PromoCode::find($state);
+                                $discount = $promoCode ? $promoCode->discount_amount : 0;
 
+                                $set('discount_amount', $discount);
+
+                                $grandTotalAmount = $subTotalAmount - $discount;
+                                $set('grand_total_amount', $grandTotalAmount);
+                            }),
+
+                            Forms\Components\TextInput::make('sub_total_amount')
+                            ->required()
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('grand_total_amount')
+                            ->required()
+                            ->readOnly()
+                            ->numeric()
+                            ->prefix('IDR'),
+
+                            Forms\Components\TextInput::make('discount_amount')
+                            ->required()
+                            ->numeric()
+                            ->prefix('IDR'),
 
                         ]),
-                    ]),
+                    Forms\Components\Fieldset::make('Customer Information')
+                        ->schema([
+                            Forms\Components\Grid::make(2)
+                                ->schema([
+                                    Forms\Components\TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    Forms\Components\TextInput::make('phone')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    Forms\Components\TextInput::make('email')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    Forms\Components\Textarea::make('address')
+                                        ->required()
+                                        ->rows(5),
+
+                                    Forms\Components\TextInput::make('city')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    Forms\Components\TextInput::make('post_code')
+                                        ->required()
+                                        ->maxLength(255),
+                                ]),
+                        ]),
+
+                Forms\Components\Fieldset::make('Payment Information')
+                    ->schema([
+
+                        Forms\Components\TextInput::make('book_trx_id')
+                            ->required()
+                            ->maxLength(255),
+
+                        Forms\Components\ToggleButtons::make('is_paid')
+                            ->label('Apakah Sudah Membayar')
+                            ->boolean()
+                            ->grouped()
+                            ->icons([
+                                true => 'heroicon-o-pencil',
+                                false => 'heroicon-o-clock',
+                            ])
+
+                            ->required(),
+
+                        Forms\Components\FileUpload::make('proof')
+                            ->image()
+                            ->required(),
+                ]),
                 ]),
             ]);
     }
@@ -87,12 +194,48 @@ class ProductTransactionResource extends Resource
         return $table
             ->columns([
                 //
+                Tables\Columns\ImageColumn::make('package.thumbnail'),
+
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+
+                Tables\Columns\TextColumn::make('book_trx_id')
+                ->searchable()
+                ->label('Kode Transaksi'),
+
+                Tables\Columns\IconColumn::make('is_paid')
+                ->boolean()
+                ->trueColor('success')
+                ->falseColor('danger')
+                ->trueIcon('heroicon-o-check-circle')
+                ->falseIcon('heroicon-o-x-circle')
+                ->label('Terverifikasi'),
             ])
             ->filters([
                 //
+                Tables\Filters\SelectFilter::make('package_id')
+                    ->label('Package')
+                    ->relationship('package', 'name'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+
+                Tables\Actions\Action::make('approve')
+                ->label('Approve')
+                ->action(function(ProductTransaction $record){
+                    $record->is_paid =true;
+                    $record->save();
+
+                    Notification::make()
+                    ->title('Order Aproved')
+                    ->success()
+                    ->body('The Order has been successfully approved.')
+                    ->send();
+                })
+                ->color('success')
+                ->requiresConfirmation()
+                ->visible(fn (ProductTransaction $record) => !$record->is_paid),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
